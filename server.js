dbb2195a770a01daecb4018bcee2daca4788b835
MUNIYARAJ B <muniyarajbalamurugan7@gmail.com>
2025-11-15 00:09:36 +0530

    Refactor user creation endpoint to use structured data
    Refactor user creation to extract relevant data from the request body before creating a user.

--- server.js
+++ server.js
@@ -12,16 +12,29 @@ const User = require("./models/user");
 const app = express();
 app.use(express.json());
 
-// CREATE (Add User)
 app.post("/users", async (req, res) => {
     try {
-         console.log("Received Webhook:", req.body);
-        const user = await User.create(req.body);
+        console.log("Received Webhook:", req.body);
+
+        const data = {
+            name: req.body.session?.name?.value || null,
+            email: req.body.session?.mail?.value || null,
+            handler: req.body.handler,
+            request: req.body.request,
+            environment: req.body.environment,
+            component: req.body.component,
+            context: req.body.context,
+            raw: req.body
+        };
+
+        const user = await User.create(data);
         res.status(201).json(user);
     } catch (err) {
+        console.error(err);
         res.status(500).json({ error: err.message });
     }
 });
+
 
 // READ ALL USERS
 app.get("/users", async (req, res) => {
